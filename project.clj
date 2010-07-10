4c432a8cb7a2020eff64d499527b9c03e32e41d6
Roman Scherer <roman@burningswell.com>
2010-07-10 13:31:10 +0200

    Changed version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject validation-clj "1.0.0-SNAPSHOT"
+(defproject validation-clj "0.1.0-SNAPSHOT"
   :description "A validation library for Clojure."
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
