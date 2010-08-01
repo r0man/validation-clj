109f02360e952b0d06954ea09f0e4ad5d6d8d692
Roman Scherer <roman@burningswell.com>
2010-08-01 14:27:14 +0200

    Switched to clojure 1.2.0-RC1.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject validation-clj "0.1.0-SNAPSHOT"
   :description "A validation library for Clojure."
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
+                 [org.clojure/clojure-contrib "1.2.0-RC1"]]
   :dev-dependencies [[autodoc "0.7.1-SNAPSHOT"]
                      [lein-clojars "0.5.0"]
                      [swank-clojure "1.2.1"]])
