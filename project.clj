b3ab8dcc4167e491dc981afa25c382452ef74cfc
Roman Scherer <roman.scherer@nugg.ad>
2011-05-11 15:49:26 +0200

    Switched to clojure 1.2.0.

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject validation-clj "0.1.0-SNAPSHOT"
   :description "A validation library for Clojure."
-  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]]
-  :dev-dependencies [[autodoc "0.7.1-SNAPSHOT"]
-                     [lein-clojars "0.5.0"]
-                     [swank-clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
