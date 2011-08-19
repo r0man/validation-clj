13c37c9041867e155ef21ccf93c0ea2786ed1703
Roman <roman@burningswell.com>
2011-08-19 21:56:53 +0200

    Switched to clojure 1.2.1.

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject validation-clj "0.1.2-SNAPSHOT"
   :description "A validation library for Clojure."
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
