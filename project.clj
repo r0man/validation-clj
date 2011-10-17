06515ffde4bf747576d5f1adbc1b6a4dd1bb1ceb
Roman <roman@burningswell.com>
2011-10-17 20:53:32 +0200

    Back to slingshot 0.2.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject validation-clj "0.2.1-SNAPSHOT"
   :description "A validation library for Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [slingshot "0.5.0"]])
+                 [slingshot "0.2.1"]])
