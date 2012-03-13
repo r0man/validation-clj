b507c2de2620773da812f0cec0a3566c6abb2b4e
Roman <roman@burningswell.com>
2012-03-13 21:13:46 +0100

    Get ready for Leiningen 2.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject validation-clj "0.3.0-SNAPSHOT"
+(defproject validation-clj/validation-clj "0.3.0-SNAPSHOT"
   :description "A validation library for Clojure."
   :dependencies [[org.clojure/clojure "1.4.0-beta4"]
-                 [slingshot "0.10.2"]])
+                 [slingshot "0.10.2"]]
+  :min-lein-version "2.0.0")
