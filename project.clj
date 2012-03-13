8d6aac1b4cffbabc1b1aab46c66d8270bf51e61b
Roman <roman@burningswell.com>
2012-03-13 21:20:56 +0100

    Started 0.4.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject validation-clj/validation-clj "0.3.0"
+(defproject validation-clj/validation-clj "0.4.0-SNAPSHOT"
   :description "A validation library for Clojure."
   :dependencies [[org.clojure/clojure "1.4.0-beta4"]
                  [slingshot "0.10.2"]]
