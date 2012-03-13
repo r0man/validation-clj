8a6820115615507f076dd27b2295752dd8d205b6
Roman <roman@burningswell.com>
2012-03-13 21:20:28 +0100

    Release 0.3.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject validation-clj/validation-clj "0.3.0-SNAPSHOT"
+(defproject validation-clj/validation-clj "0.3.0"
   :description "A validation library for Clojure."
   :dependencies [[org.clojure/clojure "1.4.0-beta4"]
                  [slingshot "0.10.2"]]
