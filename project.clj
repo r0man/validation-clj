54106dd0390b78d08e85d25015e1633cddc46e92
roman <roman@bombaclaat.lan>
2012-07-27 17:11:16 +0200

    Started 0.4.2-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject validation-clj/validation-clj "0.4.1"
+(defproject validation-clj/validation-clj "0.4.2-SNAPSHOT"
   :description "A validation library for Clojure."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
