bbcaf6ecb530ecd0ea1be0d34be3086ca18eea84
roman <roman@bombaclaat.lan>
2012-07-27 16:54:35 +0200

    Switched to slingshot 0.10.3.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject validation-clj/validation-clj "0.4.0-SNAPSHOT"
   :description "A validation library for Clojure."
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [slingshot "0.10.2"]]
-  :min-lein-version "2.0.0")
+                 [slingshot "0.10.3"]])
