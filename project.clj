8bd4cfdf2e2dc22fb333a7cfb5e46428716ed087
Roman Scherer <roman.scherer@burningswell.com>
2013-01-27 16:49:34 +0100

    Stated 0.5.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject validation-clj/validation-clj "0.5.0"
+(defproject validation-clj/validation-clj "0.5.1-SNAPSHOT"
   :description "A validation library for Clojure."
   :min-lein-version "2.0.0"
+  :url "http://github.com/r0man/validation-clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [slingshot "0.10.3"]
                  [sqlingvo "0.2.0"]])
