a3ac12a841aaa1e2bb3e05d9fb9442b49cf87da7
Roman Scherer <roman.scherer@burningswell.com>
2013-01-27 16:39:20 +0100

    Moved predicates into core.

--- src/validation/core.clj
+++ src/validation/core.clj
@@ -1,9 +1,38 @@
 (ns validation.core
   (:refer-clojure :exclude [replace])
   (:require validation)
-  (:use [clojure.string :only (blank? join replace capitalize)]
-        [slingshot.slingshot :only [throw+]]
-        validation.predicates))
+  (:require [clojure.string :refer [blank? join replace capitalize]]
+            [slingshot.slingshot :refer [throw+]]))
+
+(def ^:dynamic *email-regex*
+  #"(?i)[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+
+(defn email?
+  "Returns true if the email address is valid, otherwise false."
+  [address] (and address (re-matches *email-regex* address)))
+
+(defn latitude?
+  "Returns true if number is betweeen -90.0 and 90.0, otherwise
+  false."
+  [number]
+  (and (number? number)
+       (>= number -90.0)
+       (<= number 90.0)))
+
+(defn longitude?
+  "Returns true if number is between -180.0 and 180.0, otherwise
+  false."
+  [number]
+  (and (number? number)
+       (>= number -180.0)
+       (<= number 180.0)))
+
+(defn location?
+  "Returns true if location has valid latitude and longitude
+  coordinates, otherwise false."
+  [location & {:keys [latitude longitude]}]
+  (and (latitude? ((or latitude :latitude) location))
+       (longitude? ((or longitude :longitude) location))))
 
 (defn error-messages
   "Returns all error messages of the record."
