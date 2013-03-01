7576c4cb750cdf120438ec3334f68eeae8834ae8
Roman Scherer <roman.scherer@burningswell.com>
2013-03-01 20:34:36 +0100

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :url "http://github.com/r0man/validation-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [slingshot "0.10.3"]
-                 [sqlingvo "0.2.0"]])
+                 [sqlingvo "0.2.1-SNAPSHOT"]])
